4d7c37311bbfd3470be66f4bed5db1c1424c1b53
timothykent38 <120700588+timothykent38@users.noreply.github.com>
2023-10-26 15:44:32 -0700

    Add files via upload

--- Quest_Flight.ino
+++ Quest_Flight.ino
@@ -77,54 +77,123 @@ void Flying() {
 
   missionMillis = millis();     //Set mission clock millis, you just entered flight conditions
   // ***************** HERE IS WHERE THE EXPERIMENT CODE STARTS *************************************************
-  // wait 24 hours (this should be done automatically)
-  // turn on voltage 
-  digitalWrite(digitalPowerOn, HIGH);
-  //***** reading the voltage and current happens each minute throughout the entire experiment
-//  read oxygen(or hydrogen) and print that to the logit file
-  readHydrogen = true;  // there is probably a better way to control when we do or do not read data from the hydrogen sensor, the current way probably does not work at all 
-//  turn on air pump
-    digitalWrite(digitalPumpOn, HIGH);
-//  read voltage and hydrogen (this happens throughout the experiment every minute)
-//  turn off votage and air pump
-    digitalWrite(digitalPowerOn, LOW);
-    digitalWrite(digitalPumpOn, LOW);
-//  reverse polaroty (i have no idea how to do this)
-//  turn on votage measure current and voltage (measuring continues to happen throughout experiment
+      // wait 24 hours (this should be done automatically)
+      // turn on voltage 
       digitalWrite(digitalPowerOn, HIGH);
-//  turrn off voltage
-    digitalWrite(digitalPowerOn, LOW);
-//  turn on motor
-//  turn off motor
-//  if 30 days have passed end else go back to turn on power (we may want to put all this code inside a 1 day event so it runs every day and have a variable that counts the number of days that have passed)
-  //
-  //
-  /////////////////////////////////////////////////////
-  //----- Here to start a flight from a reset ---------
-  /////////////////////////////////////////////////////
-  //
-  DateTime now = rtc.now();                   //get time now
-  currentunix = (now.unixtime());             //get current unix time, don't count time not flying
-  writelongfram(currentunix, PreviousUnix);   //set fram Mission time start now counting seconds unix time
-  //
-  //***********************************************************************
-  //***********************************************************************
-  //  All Flight conditions are now set up,  NOW to enter flight operations
-  //
-  //
-  while (1) {
-    //
-    //----------- Test for terminal abort command from flying ----------------------
-    //
-    while (Serial.available()) {      //Abort flight program progress
-      byte x = Serial.read();         //get the input byte
-      if (x == 'x') {                 //check the byte for an abort x
-        return  ;                     //return back to poeration sellection
-      }                               //end check
-    }                                 //end abort check
-    //-------------------------------------------------------------------
-   //********* event for printing the data from voltage and current sensor
-        if ((millis() - eventDataTimer) > eventReadData) {
+      //***** reading the voltage and current happens each minute throughout the entire experiment
+    //  read oxygen(or hydrogen) and print that to the logit file
+      readHydrogen = true;  // there is probably a better way to control when we do or do not read data from the hydrogen sensor, the current way probably does not work at all 
+    //  turn on air pump
+        digitalWrite(digitalPumpOn, HIGH);
+    //  read voltage and hydrogen (this happens throughout the experiment every minute)
+    //  turn off votage and air pump
+        digitalWrite(digitalPowerOn, LOW);
+        digitalWrite(digitalPumpOn, LOW);
+    //  reverse polaroty (i have no idea how to do this)
+    //  turn on votage measure current and voltage (measuring continues to happen throughout experiment
+          digitalWrite(digitalPowerOn, HIGH);
+    //  turrn off voltage
+        digitalWrite(digitalPowerOn, LOW);
+    //  turn on motor
+    //  turn off motor
+    //  if 30 days have passed end else go back to turn on power (we may want to put all this code inside a 1 day event so it runs every day and have a variable that counts the number of days that have passed)
+      //
+      //
+      /////////////////////////////////////////////////////
+      //----- Here to start a flight from a reset ---------
+      /////////////////////////////////////////////////////
+      //
+      DateTime now = rtc.now();                   //get time now
+      currentunix = (now.unixtime());             //get current unix time, don't count time not flying
+      writelongfram(currentunix, PreviousUnix);   //set fram Mission time start now counting seconds unix time
+      //
+      //***********************************************************************
+      //***********************************************************************
+      //  All Flight conditions are now set up,  NOW to enter flight operations
+      //
+      //
+      while (1) {
+        //
+        //----------- Test for terminal abort command from flying ----------------------
+        //
+        while (Serial.available()) {      //Abort flight program progress
+          byte x = Serial.read();         //get the input byte
+          if (x == 'x') {                 //check the byte for an abort x
+            return  ;                     //return back to poeration sellection
+          }                               //end check
+        }                                 //end abort check
+        //-------------------------------------------------------------------
+       //********* event for printing the data from voltage and current 
+    
+    if ((millis() - eventDataTimer) > eventReadData) {
+              // ******* reading temp info 
+              // Example testing sketch for various DHT humidity/temperature sensors
+    // Written by ladyada, public domain
+    
+    // REQUIRES the following Arduino libraries:
+    // - DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
+    // - Adafruit Unified Sensor Lib: https://github.com/adafruit/Adafruit_Sensor
+    
+    #include "DHT.h"
+    
+    #define DHTPIN 2     // Digital pin connected to the DHT sensor
+    // Feather HUZZAH ESP8266 note: use pins 3, 4, 5, 12, 13 or 14 --
+    // Pin 15 can work but DHT must be disconnected during program upload.
+    
+    // Uncomment whatever type you're using!
+    //#define DHTTYPE DHT11   // DHT 11
+    #define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+    //#define DHTTYPE DHT21   // DHT 21 (AM2301)
+    
+    // Connect pin 1 (on the left) of the sensor to +5V
+    // NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
+    // to 3.3V instead of 5V!
+    // Connect pin 2 of the sensor to whatever your DHTPIN is
+    // Connect pin 3 (on the right) of the sensor to GROUND (if your sensor has 3 pins)
+    // Connect pin 4 (on the right) of the sensor to GROUND and leave the pin 3 EMPTY (if your sensor has 4 pins)
+    // Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
+    
+    // Initialize DHT sensor.
+    // Note that older versions of this library took an optional third parameter to
+    // tweak the timings for faster processors.  This parameter is no longer needed
+    // as the current DHT reading algorithm adjusts itself to work on faster procs.
+    DHT dht(DHTPIN, DHTTYPE);
+    
+  
+      // Wait a few seconds between measurements.
+      delay(2000);
+    
+      // Reading temperature or humidity takes about 250 milliseconds!
+      // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+      float h = dht.readHumidity();
+      // Read temperature as Celsius (the default)
+      float t = dht.readTemperature();
+      // Read temperature as Fahrenheit (isFahrenheit = true)
+      float f = dht.readTemperature(true);
+    
+      // Check if any reads failed and exit early (to try again).
+      if (isnan(h) || isnan(t) || isnan(f)) {
+        Serial.println(F("Failed to read from DHT sensor!"));
+        return;
+      }
+    
+      // Compute heat index in Fahrenheit (the default)
+      float hif = dht.computeHeatIndex(f, h);
+      // Compute heat index in Celsius (isFahreheit = false)
+      float hic = dht.computeHeatIndex(t, h, false);
+    
+      Serial.print(F("Humidity: "));
+      Serial.print(h);
+      Serial.print(F("%  Temperature: "));
+      Serial.print(t);
+      Serial.print(F("°C "));
+      Serial.print(f);
+      Serial.print(F("°F  Heat index: "));
+      Serial.print(hic);
+      Serial.print(F("°C "));
+      Serial.print(hif);
+      Serial.println(F("°F"));
+
           if(readHydrogen){
             int hydrogen = analogRead(analogReadHydrogen);  // reads the hydrogen from the sensor
             // print hydrogen to the logit file
